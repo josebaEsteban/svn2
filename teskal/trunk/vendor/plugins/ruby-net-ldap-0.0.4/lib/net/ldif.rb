6ab50f7b712614fb6cef1757c1fddfd59052303b
teskal <teskal@4807bb34-8bd1-4d29-bbbb-6352f067c437>
2007-09-04 23:19:41 +0000

    mas

---
+++ teskal/trunk/vendor/plugins/ruby-net-ldap-0.0.4/lib/net/ldif.rb
@@ -0,0 +1,41 @@
+# $Id: ldif.rb 78 2006-04-26 02:57:34Z blackhedd $
+#
+# Net::LDIF for Ruby
+#
+#
+#
+# Copyright (C) 2006 by Francis Cianfrocca. All Rights Reserved.
+#
+# Gmail: garbagecat10
+#
+
+modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+
+
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+#
+
+# THIS FILE IS A STUB.
+
+module Net
+
+  class LDIF
+
+
+  end # class LDIF
+
+
+end # module Net
+
+
