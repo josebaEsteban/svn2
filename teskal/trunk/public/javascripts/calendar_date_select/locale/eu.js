2ae8b6c14bdf24820555314fbc998dbe25de88a4
teskal <teskal@4807bb34-8bd1-4d29-bbbb-6352f067c437>
2007-09-29 19:14:55 +0000

    calendar

---
+++ teskal/trunk/public/javascripts/calendar_date_select/locale/eu.js
@@ -0,0 +1,8 @@
+_translations = {
+   "OK": "OK",
+   "Now": "Ahora",
+   "Today": "Hoy"
+ }
+ Date.first_day_of_week = 1;
+ Date.weekdays = $w("L M Mi J V S D");
+ Date.months = $w("Urtarrila  Otsaila  Martxoa  Apirila  Maiatza  Ekaina  Uztaila  Abuztua  Iraila  Urria  Azaroa  Abendua"); 
