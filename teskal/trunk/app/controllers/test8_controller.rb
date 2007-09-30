b0f07d8410fc26352c72471f6dec41f46eb0cf96
teskal <teskal@4807bb34-8bd1-4d29-bbbb-6352f067c437>
2007-09-30 14:57:42 +0000

    show

--- teskal/trunk/app/controllers/test8_controller.rb
+++ teskal/trunk/app/controllers/test8_controller.rb
@@ -1,8 +1,34 @@
 class Test8Controller < ApplicationController
-  layout 'base'
-  before_filter :require_login
+  layout 'base'   
+    before_filter :require_login
   
   def new
     @answer = Answer.new
+  end
+  
+  def create
+    @answer = Answer.new(params[:answer])
+    @answer.questionnare_id=1
+    @answer.user_id=session[:user_id]
+    if @answer.save
+      flash[:notice] = 'Answer was successfully created.'
+      redirect_to :action => 'show', :id => @answer.id
+
+      # format.html { redirect_to answer_url(@answer) }
+      # format.xml  { head :created, :location => answer_url(@answer) }
+    else
+      format.html { render :action => "new" }
+      format.xml  { render :xml => @answer.errors.to_xml }
+    end
+  end
+
+  def show
+    @answer = Answer.find(params[:id])
+    fecha = @answer.created_on
+
+    @user=User.find(@answer.user_id )
+  end
+
+  def chart
   end
 end
