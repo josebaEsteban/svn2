9b559a3ad7ae115b91db9783172062713f58a9eb
teskal <teskal@4807bb34-8bd1-4d29-bbbb-6352f067c437>
2007-10-02 03:48:10 +0000

    ready for test

--- teskal/trunk/app/controllers/welcome_controller.rb
+++ teskal/trunk/app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 # Teskal
-# Copyright (C)2007  Teskal 
+# Copyright (C)2007  Teskal
 
 
 class WelcomeController < ApplicationController
@@ -7,19 +7,19 @@ class WelcomeController < ApplicationController
 
   def index
     # @projects = Project.latest logged_in_user
-     @answers = Answer.find_by_user_id(session[:user_id])
-     puts session[:user_id] 
-     puts  @answers     
-      end  
-  
+    @answers = Answer.find_by_user_id(session[:user_id])
+    puts session[:user_id]
+    puts  @answers
+  end
+
   def en
     set_language_if_valid ('es')
     redirect_to :controller  => 'test2', :action => 'show', :id => 22
-  end 
+  end
 
-    def list
-     @answer = Answer.find_by_user_id(5)      
-     @answer_pages, @answers = paginate :answers, :per_page => 40
-    end
+  def list
+    @answers = Answer.find_by_sql("select * from answers where answers.user_id=#{session[:user_id]} order by answers.created_on DESC")
+    # @answer_pages, @answers = paginate :answers, :per_page => 40
+  end
 
 end
