42f5459577a71c5f679fcaa3fc3e9098e0d07920
teskal <teskal@4807bb34-8bd1-4d29-bbbb-6352f067c437>
2007-10-05 06:13:45 +0000

    test de quest 8 contra sifolito y cambios en lang

--- teskal/trunk/app/controllers/quest8_controller.rb
+++ teskal/trunk/app/controllers/quest8_controller.rb
@@ -35,13 +35,13 @@ class Quest8Controller < ApplicationController
     # calculo de las dimensiones
 
     rg = (@answer.answ1 + @answer.answ2  + @answer.answ3 )/3.0
-    uc = ( @answer.answ39 + @answer.answ30 + @answer.answ39  + @answer.answ42 )/4.0
+    dp = ( @answer.answ39 + @answer.answ40 + @answer.answ41  + @answer.answ42 )/4.0
     fe = ( @answer.answ32 + @answer.answ33 + @answer.answ34 )/3.0
-    dp = ( @answer.answ7 + @answer.answ8 + @answer.answ9 + @answer.answ10 + @answer.answ11  )/5.0
+    uc = ( @answer.answ7 + @answer.answ8 + @answer.answ9 + @answer.answ10 + @answer.answ11  )/5.0
     ei = ( @answer.answ23 + @answer.answ24 + @answer.answ25 )/3.0
     tp = ( @answer.answ18 + @answer.answ19 + @answer.answ20 + @answer.answ21 + @answer.answ22 )/5.0
-    td = ( @answer.answ14 + @answer.answ15 + @answer.answ16 + @answer.answ17)/4.0
-    re = ( @answer.answ4 + @answer.answ5 + @answer.answ6)/4.0
+    td = ( @answer.answ12 + @answer.answ13 + @answer.answ14 + @answer.answ15 + @answer.answ16 + @answer.answ17)/6.0
+    re = ( @answer.answ4 + @answer.answ5 + @answer.answ6)/3.0
     it = ( @answer.answ26 + @answer.answ27 + @answer.answ28)/3.0
     ct = ( @answer.answ35 + @answer.answ36 + @answer.answ37 + @answer.answ38)/4.0
 
@@ -183,19 +183,19 @@ class Quest8Controller < ApplicationController
     strXML = "<chart caption='"+l(:quest8_label_0)+"' subCaption='"+@user.login+"' yAxisName='"+@fecha.to_s+"' palette='2' yAxisMaxValue='7' showShadow='1' use3DLighting='1' legendAllowDrag='1' useRoundEdges='1' noValue='0' showValues='0' bgcolor='ffffff' borderColor='ffffff'>"
     strXML = strXML + "<set label='" + l(:quest8_label_1) + "' value='" + rgs + "'/>"
     strXML = strXML + "<set label='" + l(:quest8_label_8) + "' value='" + dps + "'/>"
-    strXML = strXML + "<set label='" + l(:quest8_label_3) + "' value='" + fes + "'/>"
+    strXML = strXML + "<set label='" + l(:quest8_label_7) + "' value='" + fes + "'/>"
     strXML = strXML + "<set label='' value=''/>"
-    strXML = strXML + "<set label='" + l(:quest8_label_2) + "' value='" + ucs + "'/>"
-    strXML = strXML + "<set label='" + l(:quest8_label_5) + "' value='" + eis + "'/>"
-    strXML = strXML + "<set label='" + l(:quest8_label_6) + "' value='" + tps + "'/>"
-    strXML = strXML + "<set label='" + l(:quest8_label_7) + "' value='" + tds + "'/>"
+    strXML = strXML + "<set label='" + l(:quest8_label_3) + "' value='" + ucs + "'/>"
+    strXML = strXML + "<set label='" + l(:quest8_label_4) + "' value='" + eis + "'/>"
+    strXML = strXML + "<set label='" + l(:quest8_label_5) + "' value='" + tps + "'/>"
+    strXML = strXML + "<set label='" + l(:quest8_label_6) + "' value='" + tds + "'/>"
     strXML = strXML + "<set label='' value=''/>"
-    strXML = strXML + "<set label='" + l(:quest8_label_8) + "' value='" + res + "'/>"
+    strXML = strXML + "<set label='" + l(:quest8_label_11) + "' value='" + res + "'/>"
     strXML = strXML + "<set label='" + l(:quest8_label_9) + "' value='" + its + "'/>"
     strXML = strXML + "<set label='" + l(:quest8_label_10) + "' value='" + cts + "'/>"
     strXML = strXML + "</chart>"
 
     #Create the chart - Pie 3D Chart with data from strXML
-    @chart1= renderChart("/charts/Bar2D.swf", "", strXML, "quest8", 550, 375, false, false)
+    @chart1= renderChart("/charts/Bar2D.swf", "", strXML, "quest8", 600, 375, false, false)
   end
 end
