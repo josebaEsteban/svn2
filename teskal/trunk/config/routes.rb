d8de2d82c6b1e62755ec2aec3d51adbaa0124b4f
teskal <teskal@4807bb34-8bd1-4d29-bbbb-6352f067c437>
2007-10-14 16:09:23 +0000

    login

--- teskal/trunk/config/routes.rb
+++ teskal/trunk/config/routes.rb
@@ -10,7 +10,8 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed by hooking up '' 
   # -- just remember to delete public/index.html.
-  map.connect '', :controller => "welcome"
+  # map.connect '', :controller => "welcome"
+  map.connect '', :controller => "account", :action  => "login"
 
  
   # map.connect 'roles/workflow/:id/:role_id/:tracker_id', :controller => 'roles', :action => 'workflow'
