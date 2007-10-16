ed81ecef17355d253f549e80e9f0e07dd259e5ed
teskal <teskal@4807bb34-8bd1-4d29-bbbb-6352f067c437>
2007-10-16 10:51:36 +0000

    11

---
+++ teskal/trunk/app/controllers/quest11_controller.rb
@@ -0,0 +1,196 @@
+class Quest11Controller < ApplicationController
+  layout 'base'
+  before_filter :require_login
+
+  def new
+    @answer = Answer.new
+  end
+
+  def create
+    @answer = Answer.new(params[:answer])
+    @answer.quest_id=11
+    @answer.user_id=session[:user_id]
+    @answer.ip = request.remote_ip
+    if @answer.save
+      # flash[:notice] = 'Answer was successfully created.'
+      redirect_to :action => 'show', :id => @answer.id
+
+      # format.html { redirect_to answer_url(@answer) }
+      # format.xml  { head :created, :location => answer_url(@answer) }
+    else
+      format.html { render :action => "new" }
+      format.xml  { render :xml => @answer.errors.to_xml }
+    end
+  end
+
+  def show
+    @answer = Answer.find(params[:id])
+    @fecha = l_datetime(@answer.created_on)
+    require_coach(@answer.user_id)
+    @user=User.find(@answer.user_id )
+    teskalChart11
+  end
+
+
+  def teskalChart11
+    @advice=[]
+    @icon=[]
+
+    toKnow = (@answer.answ2 + @answer.answ5 + @answer.answ26 + @answer.answ30)/4.0
+    item=0
+    # if toKnow < 2.5
+    #   @advice[item]=l(:quest11_d1_a)
+    #   @icon[item]="stop"
+    # else
+    #   if toKnow < 4
+    #     @advice[item]=l(:quest11_d1_b)
+    #     @icon[item]="medium"
+    #   else
+    #     @advice[item]=l(:quest11_d1_c)
+    #     @icon[item]="star"
+    #   end
+    # end
+    toAccomplish = (@answer.answ9 + @answer.answ13 + @answer.answ16 + @answer.answ22)/4.0
+    item=1
+    # if toAccomplish < 2.5
+    #   @advice[item]=l(:quest11_d2_a)
+    #   @icon[item]="stop"
+    # else
+    #   if toAccomplish < 4
+    #     @advice[item]=l(:quest11_d2_b)
+    #     @icon[item]="medium"
+    #   else
+    #     @advice[item]=l(:quest11_d2_c)
+    #     @icon[item]="star"
+    #   end
+    # end
+    toExperience = (@answer.answ1 + @answer.answ14 + @answer.answ20 + @answer.answ28)/4.0
+    item=2
+    # if toExperience < 2.5
+    #   @advice[item]=l(:quest11_d3_a)
+    #   @icon[item]="stop"
+    # else
+    #   if toExperience < 4
+    #     @advice[item]=l(:quest11_d3_b)
+    #     @icon[item]="medium"
+    #   else
+    #     @advice[item]=l(:quest11_d3_c)
+    #     @icon[item]="star"
+    #   end
+    # end
+    integratedRegulation = (@answer.answ4 + @answer.answ18 + @answer.answ24 + @answer.answ31)/4.0
+    item=3
+    # if integratedRegulation < 2.5
+    #   @advice[item]=l(:quest11_d4_a)
+    #   @icon[item]="stop"
+    # else
+    #   if integratedRegulation < 4
+    #     @advice[item]=l(:quest11_d4_b)
+    #     @icon[item]="medium"
+    #   else
+    #     @advice[item]=l(:quest11_d4_c)
+    #     @icon[item]="star"
+    #   end
+    # end
+    identifiedRegulation = (@answer.answ8 + @answer.answ12 + @answer.answ19 + @answer.answ27)/4.0
+    item=4
+    # if identifiedRegulation < 2.5
+    #   @advice[item]=l(:quest11_d5_a)
+    #   @icon[item]="stop"
+    # else
+    #   if identifiedRegulation < 4
+    #     @advice[item]=l(:quest11_d5_b)
+    #     @icon[item]="medium"
+    #   else
+    #     @advice[item]=l(:quest11_d5_c)
+    #     @icon[item]="star"
+    #   end
+    # end
+    introjectedRegulation = (@answer.answ10 + @answer.answ15 + @answer.answ23 + @answer.answ29)/4.0 
+    item=5
+    # if introjectedRegulation < 2.5
+    #   @advice[item]=l(:quest11_d6_a)
+    #   @icon[item]="stop"
+    # else
+    #   if introjectedRegulation < 4
+    #     @advice[item]=l(:quest11_d6_b)
+    #     @icon[item]="medium"
+    #   else
+    #     @advice[item]=l(:quest11_d6_c)
+    #     @icon[item]="star"
+    #   end
+    # end
+    externalRegulation = (@answer.answ7 + @answer.answ11 + @answer.answ17 + @answer.answ25)/4.0 
+    item=6
+    # if externalRegulation < 2.5
+    #   @advice[item]=l(:quest11_d7_a)
+    #   @icon[item]="stop"
+    # else
+    #   if externalRegulation < 4
+    #     @advice[item]=l(:quest11_d7_b)
+    #     @icon[item]="medium"
+    #   else
+    #     @advice[item]=l(:quest11_d7_c)
+    #     @icon[item]="star"
+    #   end
+    # end
+    amotivation = (@answer.answ3 + @answer.answ6 + @answer.answ21 + @answer.answ32)/4.0
+    item=7
+    # if amotivation < 2.5
+    #   @advice[item]=l(:quest11_d8_a)
+    #   @icon[item]="stop"
+    # else
+    #   if amotivation < 4
+    #     @advice[item]=l(:quest11_d8_b)
+    #     @icon[item]="medium"
+    #   else
+    #     @advice[item]=l(:quest11_d8_c)
+    #     @icon[item]="star"
+    #   end
+    # end
+
+    #strXML will be used to store the entire XML document generated
+    strXML=''
+    #Generate the chart element
+    # labelDisplay='Stagger' staggerLines='2'
+    # strXML = "<chart palette='2' caption='"+l(:quest11_label_0)+"' subCaption='"+@user.login+"' xAxisName='"+@fecha.to_s+"'showvalues='0'  decimalSeparator=',' formatNumberScale='0' legendAllowDrag='1' yAxisMinValue='0' yAxisMaxValue='7' showShadow='1'  useRoundEdges='1' showAlternateHGridColor='1' alternateHGridColor='f8f6f4' bgcolor='ffffff' borderColor='ffffff' chartLeftMargin='25' chartRightMargin='35'>"
+    # strXML = strXML +"<categories>"
+    # strXML = strXML + "<category label= '"+l(:quest11_label_1)+"'/>"
+    # strXML = strXML + "<category label= '"+l(:quest11_label_2)+"'/>"
+    # strXML = strXML + "<category label= '"+l(:quest11_label_3)+"'/>"
+    # strXML = strXML + "<category label= '"+l(:quest11_label_4)+"'/>"
+    # strXML = strXML + "<category label= '"+l(:quest11_label_5)+"'/>"
+    # strXML = strXML + "<category label= '"+l(:quest11_label_6)+"'/>"
+    # strXML = strXML + "<category label= '"+l(:quest11_label_7)+"'/>"
+    # strXML = strXML + "<category label= '"+l(:quest11_label_8)+"'/>"
+    # strXML = strXML +"</categories>"
+    # strXML = strXML +"<dataset SeriesName='"+l(:quest3_label_9)+"' lineThickness='3' renderAs='Area' >"
+    # strXML = strXML +"<set value='2.5' /><set value='5' /><set value='5' /><set value='1' /><set value='5' /><set value='5' /><set value='5' /><set value='3' />"
+    # strXML = strXML +"</dataset>"
+    # strXML = strXML +"<dataset SeriesName='"+l(:quest3_label_10)+"' lineThickness='4' renderAs='Line' >"
+    # strXML = strXML + "<set value='" + acorta(toKnow) + "'/>"
+    # strXML = strXML + "<set value='" + acorta(toAccomplish) + "'/>"
+    # strXML = strXML + "<set value='" + acorta(toExperience) + "'/>"
+    # strXML = strXML + "<set value='" + acorta(introjectedRegulation) + "'/>"
+    # strXML = strXML + "<set value='" + acorta(identifiedRegulation) + "'/>"
+    # strXML = strXML + "<set value='" + acorta(identifiedRegulation)+ "'/>"
+    # strXML = strXML + "<set value='" + acorta(externalRegulation) + "'/>"
+    # strXML = strXML + "<set value='" + acorta(amotivation) + "'/>"
+    # strXML = strXML + "</dataset> </chart>"
+    # @chart1= renderChart("/charts/MSCombi2D.swf"+l(:PBarLoadingText), "", strXML, "quest11", 750, 300, false, false)
+
+    strXML = "<chart caption='"+l(:quest11_label_0)+"' subCaption='"+@user.login+"' yAxisName='"+@fecha.to_s+"' palette='2' yAxisMaxValue='7' showShadow='1' use3DLighting='1' legendAllowDrag='1' useRoundEdges='1' noValue='0' showValues='0' bgcolor='ffffff' borderColor='ffffff'>"
+    strXML = strXML + "<set label='" + l(:quest11_label_1) + "' value='" + acorta(toKnow) + "'/>"
+    strXML = strXML + "<set label='" + l(:quest11_label_2) + "' value='" + acorta(toAccomplish) + "'/>"
+    strXML = strXML + "<set label='" + l(:quest11_label_3) + "' value='" + acorta(toExperience) + "'/>"
+    strXML = strXML + "<set label='" + l(:quest11_label_4) + "' value='" + acorta(integratedRegulation) + "'/>" 
+    strXML = strXML + "<set label='" + l(:quest11_label_5) + "' value='" + acorta(identifiedRegulation) + "'/>"
+    strXML = strXML + "<set label='" + l(:quest11_label_6) + "' value='" + acorta(introjectedRegulation) + "'/>"
+    strXML = strXML + "<set label='" + l(:quest11_label_7) + "' value='" + acorta(externalRegulation) + "'/>"
+    strXML = strXML + "<set label='" + l(:quest11_label_8) + "' value='" + acorta(amotivation) + "'/>"
+    strXML = strXML + "</chart>"
+
+    #Create the chart - Pie 3D Chart with data from strXML
+    @chart1= renderChart("/charts/Bar2D.swf"+l(:PBarLoadingText), "", strXML, "quest8", 600, 300, false, false)
+  end
+end
