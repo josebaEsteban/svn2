240d3a1d20e771204cdac1633f558e40e3a9bf32
teskal <teskal@4807bb34-8bd1-4d29-bbbb-6352f067c437>
2007-10-16 18:39:00 +0000

    layout

--- teskal/trunk/app/controllers/application.rb
+++ teskal/trunk/app/controllers/application.rb
@@ -95,12 +95,14 @@ class ApplicationController < ActionController::Base
     end
     true
   end
+
   def require_coach(user)
-     if user != session[:user_id]
+    if user != session[:user_id]
       flash[:notice] = l(:notice_not_authorized)
       redirect_back_or_default :controller => 'my', :action => 'page'
     end
   end
+
   # authorizes the user for the requested action.
   def authorize(ctrl = params[:controller], action = params[:action])
     unless @project.active?
