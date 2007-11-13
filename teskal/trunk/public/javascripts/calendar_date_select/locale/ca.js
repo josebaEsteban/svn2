d9370f6e23ff750152e9ea270f719408c3b4de0b
teskal <teskal@4807bb34-8bd1-4d29-bbbb-6352f067c437>
2007-11-13 04:54:07 +0000

    signup

--- teskal/trunk/public/javascripts/calendar_date_select/locale/ca.js
+++ teskal/trunk/public/javascripts/calendar_date_select/locale/ca.js
@@ -4,5 +4,5 @@ _translations = {
    "Today": "Avui"
  }
  Date.first_day_of_week = 1;
- Date.weekdays = $w("D D D D D D D");
+ Date.weekdays = $w("Dl	Dt	Dm	Dj	Dv	Ds	Dg");
  Date.months = $w("Gener Febrer Març Abril Maig Juny Juliol Agost Setembre Octubre Novembre Desembre"); 
