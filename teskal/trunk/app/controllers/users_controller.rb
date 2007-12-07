bdf1d15800732cd763cb1f157925fa465f3b1901
teskal <teskal@4807bb34-8bd1-4d29-bbbb-6352f067c437>
2007-12-07 18:25:54 +0000

    users

--- teskal/trunk/app/controllers/users_controller.rb
+++ teskal/trunk/app/controllers/users_controller.rb
@@ -4,54 +4,58 @@
 
 class UsersController < ApplicationController
   layout 'base'
-  before_filter :require_admin
+  before_filter :require_coach
 
   helper :sort
   include SortHelper
 
   def index
-    list
-    render :action => 'list' unless request.xhr?
+    if @logged_in_user.admin?
+      list
+      render :action => 'list' unless request.xhr?
+    end
   end
 
   def list
-    sort_init 'login', 'asc'
-    sort_update
+    if @logged_in_user.admin?
+      sort_init 'login', 'asc'
+      sort_update
 
-    @status = params[:status] ? params[:status].to_i : 1
-    conditions = nil
-    conditions = ["status=?", @status] unless @status == 0
+      @status = params[:status] ? params[:status].to_i : 1
+      conditions = nil
+      conditions = ["status=?", @status] unless @status == 0
 
-    @user_count = User.count(:conditions => conditions)
-    @user_pages = Paginator.new self, @user_count,
-    15,
-    params['page']
-    @users =  User.find :all,:order => sort_clause,
-    :conditions => conditions,
-    :limit  =>  @user_pages.items_per_page,
-    :offset =>  @user_pages.current.offset
+      @user_count = User.count(:conditions => conditions)
+      @user_pages = Paginator.new self, @user_count,
+      15,
+      params['page']
+      @users =  User.find :all,:order => sort_clause,
+      :conditions => conditions,
+      :limit  =>  @user_pages.items_per_page,
+      :offset =>  @user_pages.current.offset
 
-    render :action => "list", :layout => false if request.xhr?
+      render :action => "list", :layout => false if request.xhr?
+    end
   end
 
   def add
-    if request.get?  
+    if request.get?
       puts "request.get?"
       @user = User.new(:language => Setting.default_language)
     else
-       puts "params"
-    @user = User.new(params[:user])
-    @user.admin = params[:user][:admin] || false
-    @user.login = params[:user][:login]
-    @user.password, @user.password_confirmation = params[:password], params[:password_confirmation]
-    ip = request.remote_ip
-    @user.managed_by = session[:user_id]
-    if @user.save
-      Mailer.deliver_account_information(@user, params[:password]) 
-      # if params[:send_information]
-      flash[:notice] = l(:notice_successful_create)
-      redirect_to :action => 'list'
-    end
+      puts "params"
+      @user = User.new(params[:user])
+      @user.admin = params[:user][:admin] || false
+      @user.login = params[:user][:login]
+      @user.password, @user.password_confirmation = params[:password], params[:password_confirmation]
+      ip = request.remote_ip
+      @user.managed_by = session[:user_id]
+      if @user.save
+        Mailer.deliver_account_information(@user, params[:password])
+        # if params[:send_information]
+        flash[:notice] = l(:notice_successful_create)
+        redirect_to :action => 'list'
+      end
     end
   end
 
@@ -75,30 +79,36 @@ class UsersController < ApplicationController
   end
 
   def edit_membership
-    @user = User.find(params[:id])
-    @membership = params[:membership_id] ? Member.find(params[:membership_id]) : Member.new(:user => @user)
-    @membership.attributes = params[:membership]
-    if request.post? and @membership.save
-      flash[:notice] = l(:notice_successful_update)
+    if @logged_in_user.admin?
+      @user = User.find(params[:id])
+      @membership = params[:membership_id] ? Member.find(params[:membership_id]) : Member.new(:user => @user)
+      @membership.attributes = params[:membership]
+      if request.post? and @membership.save
+        flash[:notice] = l(:notice_successful_update)
+      end
+      redirect_to :action => 'edit', :id => @user and return
     end
-    redirect_to :action => 'edit', :id => @user and return
   end
 
   def destroy_membership
-    @user = User.find(params[:id])
-    if request.post? and Member.find(params[:membership_id]).destroy
-      flash[:notice] = l(:notice_successful_update)
+    if @logged_in_user.admin?
+      @user = User.find(params[:id])
+      if request.post? and Member.find(params[:membership_id]).destroy
+        flash[:notice] = l(:notice_successful_update)
+      end
+      redirect_to :action => 'edit', :id => @user and return
     end
-    redirect_to :action => 'edit', :id => @user and return
   end
 
-  def destroy
-    User.find(params[:id]).destroy
-    redirect_to :action => 'list'
-  rescue
-    flash[:notice] = "Unable to delete user"
-    redirect_to :action => 'list'
-  end
+  # def destroy
+  #   if @logged_in_user.admin?
+  #     User.find(params[:id]).destroy
+  #     redirect_to :action => 'list'
+  #   rescue
+  #     flash[:notice] = "Unable to delete user"
+  #     redirect_to :action => 'list'
+  #   end
+  # end
 
   def switch
     user = User.find(params[:id])
