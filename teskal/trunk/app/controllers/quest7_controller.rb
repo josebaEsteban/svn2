eac3b2b662b3839eaece8e01e46d78846f44eb76
teskal <teskal@4807bb34-8bd1-4d29-bbbb-6352f067c437>
2007-12-18 01:03:29 +0000

    cambios todos quest

--- teskal/trunk/app/controllers/quest7_controller.rb
+++ teskal/trunk/app/controllers/quest7_controller.rb
@@ -21,6 +21,9 @@ class Quest7Controller < ApplicationController
     @answer.quest_id=7
     if user.filled_for == session[:user_id]
       @answer.user_id=session[:user_id]
+      if user.show?
+        @answer.browse = true
+      end
     else
       @answer.user_id = user.filled_for
     end
