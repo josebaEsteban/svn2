3a9d9bad47b5cf2f2e08bb97ac21d8be711614eb
teskal <teskal@4807bb34-8bd1-4d29-bbbb-6352f067c437>
2007-12-20 21:55:41 +0000

    quest 9 y 10

--- teskal/trunk/app/controllers/quest10_controller.rb
+++ teskal/trunk/app/controllers/quest10_controller.rb
@@ -70,14 +70,14 @@ class Quest10Controller < ApplicationController
     item=0
     if peg < 2.5
       @advice[item]=l(:quest9_d1_a)
-      @icon[item]="stop"
+      @icon[item]="star"
     else
       if peg < 4
         @advice[item]=l(:quest9_d1_b)
         @icon[item]="medium"
       else
         @advice[item]=l(:quest9_d1_c)
-        @icon[item]="star"
+        @icon[item]="stop"
       end
     end
     maestria_grupal = (@answer.answ4 + @answer.answ21)/2.0
@@ -182,14 +182,14 @@ class Quest10Controller < ApplicationController
     item=8
     if per < 2.5
       @advice[item]=l(:quest9_d9_a)
-      @icon[item]="stop"
+      @icon[item]="star"
     else
       if per < 4
         @advice[item]=l(:quest9_d9_b)
         @icon[item]="medium"
       else
         @advice[item]=l(:quest9_d9_c)
-        @icon[item]="star"
+        @icon[item]="stop"
       end
     end
 
@@ -210,7 +210,7 @@ class Quest10Controller < ApplicationController
     strXML = strXML + "<category label= '"+l(:quest9_label_9)+"'/>"
     strXML = strXML +"</categories>"
     strXML = strXML +"<dataset SeriesName='"+l(:quest3_label_9)+"' lineThickness='3' renderAs='Area' >"
-    strXML = strXML +"<set value='5' /><set value='5' /><set value='5' /><set value='5' /><set value='1' /><set value='5' /><set value='5' /><set value='5' /><set value='3' />"
+    strXML = strXML +"<set value='1' /><set value='5' /><set value='5' /><set value='5' /><set value='1' /><set value='5' /><set value='5' /><set value='5' /><set value='3' />"
     strXML = strXML +"</dataset>"
     strXML = strXML +"<dataset SeriesName='"+l(:quest3_label_10)+"' lineThickness='4' renderAs='Line' >"
     strXML = strXML + "<set value='" + acorta(peg) + "'/>"
