217b513da1d79198da56b7b97706598f71703106
teskal <teskal@4807bb34-8bd1-4d29-bbbb-6352f067c437>
2008-02-03 17:38:49 +0000

    new brank for testing iPhone and new features

---
+++ teskal/branches/testq/app/controllers/welcome_controller.rb
@@ -0,0 +1,22 @@
+# Copyright (C)2007  Teskal
+
+
+class WelcomeController < ApplicationController
+  layout 'base'
+    skip_before_filter :check_if_login_required, :only => [:signup_done, :index] 
+  def index
+    # @projects = Project.laquest logged_in_user
+    # @answers = Answer.find_by_user_id('21')
+    redirect_to :controller => 'account', :action => 'login'
+
+  end
+
+  def list
+    @answers = Answer.find_by_sql("select * from answers where answers.user_id=#{session[:user_id]} order by answers.created_on DESC")
+    # @answer_pages, @answers = paginate :answers, :per_page => 40
+  end
+
+  def signup_done
+  end
+
+end
---
+++ teskal/branches/testq/app/helpers/quest1_helper.rb
@@ -0,0 +1,2 @@
+module Quest1Helper
+end
