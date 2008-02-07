a4297d24105c3be7640db649f49ad5c05deac896
teskal <teskal@4807bb34-8bd1-4d29-bbbb-6352f067c437>
2008-02-07 18:12:57 +0000

    again

--- teskal/branches/stable/trunk/public/javascripts/calendar_date_select/locale/fr.js
+++
@@ -1,8 +0,0 @@
-_translations = {
-   "OK": "OK",
-   "Now": "Maintenant",
-   "Today": "Aujourd'hui"
- }
- Date.first_day_of_week = 1;
- Date.weekdays = $w("L M Me J V S D");
- Date.months = $w("Janvier Février Mars Avril Mai Juin Juillet Août Septembre Octobre Novembre Décembre"); 
