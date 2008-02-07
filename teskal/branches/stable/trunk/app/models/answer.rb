54bad5c9bf9b6555b875dfedbc5dae813a22c2f5
teskal <teskal@4807bb34-8bd1-4d29-bbbb-6352f067c437>
2008-02-07 18:11:14 +0000

    again to stable

---
+++ teskal/branches/stable/trunk/app/models/answer.rb
@@ -0,0 +1,18 @@
+class Answer < ActiveRecord::Base
+  belongs_to :user
+  # before_create :fix_timezone
+  # tz_time_attributes :created_on 
+  # def created_on
+  #     d = read_attribute(:created_on)
+  #     puts d
+  #     if d.utc?
+  #       write_attribute :created_on, TzTime.at(Time.at(TzTime.zone.utc_to_local(d)))
+  #     else
+  #       d
+  #     end
+  #   end 
+  # protected
+  #   def fix_timezone
+  #     self.created_on = TzTime.at(created_on)
+  #   end  
+end
