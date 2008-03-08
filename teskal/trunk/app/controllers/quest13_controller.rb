8935f0dac35b48b951e43c0de71822759cc13987
teskal <teskal@4807bb34-8bd1-4d29-bbbb-6352f067c437>
2008-03-08 19:49:13 +0000

    new quest 13 and bugs in quests html tables

--- teskal/trunk/app/controllers/quest13_controller.rb
+++ teskal/trunk/app/controllers/quest13_controller.rb
@@ -1,4 +1,4 @@
-class Quest10Controller < ApplicationController
+class Quest13Controller < ApplicationController
   layout 'base'
   before_filter :require_login, :require_suscription
 
@@ -36,51 +36,21 @@ class Quest10Controller < ApplicationController
 
     #Generate the chart element
     # labelDisplay='Stagger' staggerLines='2'
-    strXML = "<chart palette='2' caption='"+l(:quest10_label_0)+"' subCaption='"+@user.name+"' xAxisName='"+@fecha.to_s+"'showvalues='0'  decimalSeparator=',' formatNumberScale='0' legendAllowDrag='1' yAxisMinValue='0' yAxisMaxValue='10' showShadow='1'  useRoundEdges='1' showAlternateHGridColor='1' alternateHGridColor='f8f6f4' bgcolor='ffffff' borderColor='ffffff' chartLeftMargin='25' chartRightMargin='35'>"
+    strXML = "<chart palette='2' caption='"+l(:quest13_label_0)+"' subCaption='"+@user.name+"' xAxisName='"+@fecha.to_s+"'showvalues='0'  decimalSeparator=',' formatNumberScale='0' legendAllowDrag='1' yAxisMinValue='0' yAxisMaxValue='10' showShadow='1'  useRoundEdges='1' showAlternateHGridColor='1' alternateHGridColor='f8f6f4' bgcolor='ffffff' borderColor='ffffff' chartLeftMargin='25' chartRightMargin='35'>"
     strXML = strXML +"<categories>"
-    strXML = strXML + "<category label= '"+l(:quest9_label_1)+"'/>"
-    strXML = strXML + "<category label= '"+l(:quest9_label_2)+"'/>"
+    strXML = strXML + "<category label= '"+l(:quest9_label_9)+"'/>"
     strXML = strXML + "<category label= '"+l(:quest9_label_3)+"'/>"
     strXML = strXML + "<category label= '"+l(:quest9_label_4)+"'/>"
     strXML = strXML + "<category label= '"+l(:quest9_label_5)+"'/>"
     strXML = strXML + "<category label= '"+l(:quest9_label_6)+"'/>"
     strXML = strXML + "<category label= '"+l(:quest9_label_7)+"'/>"
     strXML = strXML + "<category label= '"+l(:quest9_label_8)+"'/>"
-    strXML = strXML + "<category label= '"+l(:quest9_label_9)+"'/>"
     strXML = strXML +"</categories>"
     strXML = strXML +"<dataset SeriesName='"+l(:quest3_label_9)+"' lineThickness='3' renderAs='Area' >"
-    strXML = strXML +"<set value='"+(1*scale).to_s+"'/><set value='"+(5*scale).to_s+"' /><set value='"+(5*scale).to_s+"'/><set value='"+(5*scale).to_s+"'/><set value='"+(1*scale).to_s+"'/><set value='"+(5*scale).to_s+"'/><set value='"+(5*scale).to_s+"'/><set value='"+(5*scale).to_s+"'/><set value='"+(3*scale).to_s+"'/>"
+    strXML = strXML +"<set value='"+(3*scale).to_s+"'/><set value='"+(5*scale).to_s+"'/><set value='"+(5*scale).to_s+"'/><set value='"+(1*scale).to_s+"'/><set value='"+(5*scale).to_s+"'/><set value='"+(5*scale).to_s+"'/><set value='"+(5*scale).to_s+"'/>"
     strXML = strXML +"</dataset>"
 
     for answer in @answers
-      peg = ((answer.answ3 + answer.answ5)) /2.0
-      item=0
-      if peg < valor_bajo
-        @advice[item]=l(:quest9_d1_a)
-        @icon[item]="star"
-      else
-        if peg < valor_medio
-          @advice[item]=l(:quest9_d1_b)
-          @icon[item]="medium"
-        else
-          @advice[item]=l(:quest9_d1_c)
-          @icon[item]="stop"
-        end
-      end
-      maestria_grupal = ((answer.answ4 + answer.answ21)/2.0)
-      item=1
-      if maestria_grupal < valor_bajo
-        @advice[item]=l(:quest9_d2_a)
-        @icon[item]="stop"
-      else
-        if maestria_grupal < valor_medio
-          @advice[item]=l(:quest9_d2_b)
-          @icon[item]="medium"
-        else
-          @advice[item]=l(:quest9_d2_c)
-          @icon[item]="star"
-        end
-      end
       co = ((answer.answ16 + answer.answ17 + answer.answ19 + answer.answ20)/4.0)
       item=2
       if co < valor_bajo
@@ -186,18 +156,16 @@ class Quest10Controller < ApplicationController
       if answer.id == @id
         strXML = strXML+l(:quest3_label_10)+"' lineThickness='4' "
       else
-        strXML = strXML+l_datetime(TzTime.zone.utc_to_local(answer.created_on))+"' anchorSides='4' lineThickness='1' "
+        strXML = strXML+l_datetime(TzTime.zone.utc_to_local(answer.created_on))+"' anchorSides='5' lineThickness='1' "
       end
       strXML = strXML +" renderAs='Line' >"
-      strXML = strXML + "<set value='" + acorta(peg * scale) + "'/>"
-      strXML = strXML + "<set value='" + acorta(maestria_grupal * scale) + "'/>"
+      strXML = strXML + "<set value='" + acorta(per * scale) + "'/>"
       strXML = strXML + "<set value='" + acorta(co * scale) + "'/>"
       strXML = strXML + "<set value='" + acorta(ca * scale) + "'/>"
       strXML = strXML + "<set value='" + acorta(cr * scale) + "'/>"
       strXML = strXML + "<set value='" + acorta(ci * scale) + "'/>"
       strXML = strXML + "<set value='" + acorta(ed * scale)+ "'/>"
       strXML = strXML + "<set value='" + acorta(pem * scale) + "'/>"
-      strXML = strXML + "<set value='" + acorta(per * scale) + "'/>"
       strXML = strXML + "</dataset>"
     end
     strXML = strXML + "</chart>"
