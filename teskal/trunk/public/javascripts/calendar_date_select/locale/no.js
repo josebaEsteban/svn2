48730f7b20adc727f8b1dd51e57377198fdac59c
teskal <teskal@4807bb34-8bd1-4d29-bbbb-6352f067c437>
2008-05-20 11:37:10 +0000

    noruego validation

---
+++ teskal/trunk/public/javascripts/calendar_date_select/locale/no.js
@@ -0,0 +1,8 @@
+_translations = {
+   "OK": "OK",
+   "Now": "Nå",
+   "Today": "I dag"
+ }
+ Date.first_day_of_week = 1;
+ Date.weekdays = $w("M Ti O T F L S");
+ Date.months = $w("Januar Februar Mars April Mai Juni Juli August September Oktober November Desember"); 
